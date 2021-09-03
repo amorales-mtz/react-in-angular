bbb1f09a4ad81176ad154069045edf4d8727ca63
Alberto Morales <amorales@gcpglobal.com>
2021-09-03 16:56:05 -0500

    feat: Add Routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ReactWrapperComponent } from 'src/app/components/ReactWrapperComponent';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
