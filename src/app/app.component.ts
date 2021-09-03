e9b977395f0e463689788c922d0778c1f279e99e
Alberto Morales <amorales@gcpglobal.com>
2021-09-03 17:36:46 -0500

    feat: Scrap HTML from Dash App standalone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { parse } from 'node-html-parser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'react-in-angular';
 
-  counter = 666;
+  counter = 10;
+
+
+  /**
+   * Initialization.
+   * Performs complex initializations and fetch initial data.
+   */
+  ngOnInit(): void {
+    // Get the entire Dash HTML web page
+    (async () => {
+      const response = await fetch('./dash_app_standalone.html');
+      const text = await response.text();
+      // Convert the string into HTML
+      const dashHTML = parse(text);
+      // Get the HTML element
+      const element = dashHTML.querySelector('#react-entry-point').innerHTML;
+      const nextElement = dashHTML.querySelector('#react-entry-point').nextElementSibling.innerHTML;
+      console.log(element);
+    })();
+  }
 
   handleOnClick(stateCounter: number) {
     this.counter++;
