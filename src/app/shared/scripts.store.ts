79561aad28ac266b0f486680030010796c49d0fd
Julio González <jgonzalez@gcpglobal.com>
2021-09-07 17:48:06 -0500

    feat(dash): updated renderer

--- src/app/shared/scripts.store.ts
+++ src/app/shared/scripts.store.ts
@@ -1,15 +1,15 @@
 import { Scripts } from '../models/script.model';
 
 export const ScriptStore: Scripts[] = [
-    {name: 'dashDeps1', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/polyfill@7.v1_21_0m1630641792.12.1.min.js'},
-    {name: 'dashDeps2', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/react@16.v1_21_0m1630641792.14.0.min.js'},
-    {name: 'dashDeps3', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/react-dom@16.v1_21_0m1630641792.14.0.min.js'},
-    {name: 'dashDeps4', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/prop-types@15.v1_21_0m1630641792.7.2.min.js'},
-    {name: 'dashDeps5', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_html_components/dash_html_components.v1_1_4m1630641790.min.js'},
-    {name: 'dashDeps6', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_table/bundle.v4_12_0m1630641789.js'},
-    {name: 'dashDeps7', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_core_components/dash_core_components.v1_17_1m1630641790.js'},
-    {name: 'dashDeps8', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_core_components/dash_core_components-shared.v1_17_1m1630641790.js'},
-    {name: 'dashDeps9', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_bootstrap_components/_components/dash_bootstrap_components.v0_13_0m1630641796.min.js'},
-    {name: 'dashDeps10', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/dash-renderer/build/dash_renderer.v1_21_0m1630641792.min.js'},
+    {name: 'dashDeps1', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/polyfill@7.v2_0_0m1630896048.12.1.min.js'},
+    {name: 'dashDeps2', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/react@16.v2_0_0m1630896048.14.0.min.js'},
+    {name: 'dashDeps3', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/react-dom@16.v2_0_0m1630896048.14.0.min.js'},
+    {name: 'dashDeps4', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/deps/prop-types@15.v2_0_0m1630896048.7.2.min.js'},
+    {name: 'dashDeps5', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash_bootstrap_components/_components/dash_bootstrap_components.v0_13_0m1630896063.min.js'},
+    {name: 'dashDeps6', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/dash-renderer/build/dash_renderer.v2_0_0m1630896048.min.js'},
+    {name: 'dashDeps7', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/dcc/dash_core_components.v2_0_0m1630896048.js'},
+    {name: 'dashDeps8', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/dcc/dash_core_components-shared.v2_0_0m1630896048.js'},
+    {name: 'dashDeps9', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/html/dash_html_components.v2_0_0m1630896048.min.js'},
+    {name: 'dashDeps10', src: 'https://poc-dash-oa.azurewebsites.net/_dash-component-suites/dash/dash_table/bundle.v5_0_0m1630896048.js'},
     {name: 'dashRenderer', src: 'assets/dashRenderer.js'},
 ];
